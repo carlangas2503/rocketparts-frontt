e915ded87a9a2140f994e8e68d92ace2a086c0eb
Akadus23 <davilgo23@gmail.com>
2023-08-10 15:06:31 -0500

    proteccion de rutas y perfil

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useAuth0 } from "@auth0/auth0-react";
 import { usuarioID } from "../../redux/actions";
+import Redirect from "../../components/Redirect/Redirect";
 
 export default function Profile() {
+
+    const dispatch = useDispatch();
+    const usuario = useSelector((state) => state.usuarioDetail)
     const { user, isAuthenticated } = useAuth0();
+    
+    console.log(user);
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            const userData = {
-                name: user.name,
-                nickname: user.nickname,
-                email: user.email,
-                picture: user.picture,
-                sub: user.sub,
-            }
-        }
-    })
-
-
-    return (
-        <div class='text-zinc-100'>
-        {/* <img src={user.picture} alt='' ></img> */}
-        <h1>{user.name} </h1>
-        <h2>{user.nickname} </h2>
-        </div>
-    )
+    if(isAuthenticated){
+        return (
+            <div class='text-zinc-100 w-full h-screen grid-cols-1 justify-center items-center'>
+                <div class='w-96 h-96 m-auto bg-zinc-800 drop-shadow-lg  items-center justify-center flex flex-col rounded-3xl gap-4 text-lg'>
+                    <img class='w-48 h-48 rounded-full border-4 border-violet-600' src={user.picture} alt='' ></img>
+                    <p>Nickname: {user.nickname} </p>
+                    <p>Nombre: {user.name} </p>
+                    <p>Correo: {user.email} </p>
+                    </div>
+            </div>
+        )
+    } else {
+        return <Redirect/>
+    }
 }
